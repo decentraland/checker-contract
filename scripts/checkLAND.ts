c578f5f513f5acaf3710627019ac5981b9b4291f
Nando <24811313+fzavalia@users.noreply.github.com>
2023-02-09 12:03:44 -0300

    feat: Add test like assertions to script

--- scripts/checkLAND.ts
+++ scripts/checkLAND.ts
@@ -2,30 +2,75 @@ import hre, { ethers } from "hardhat";
 import { Checker__factory } from "../typechain-types";
 import { bytecode } from "./bytecode.json";
 
+const checkerAddress = ethers.Wallet.createRandom().address;
+const checkerInterface = Checker__factory.createInterface();
+
+const contracts = {
+  land: "0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d",
+  estate: "0x959e104e1a4db6317fa58f8295f586e1a978c297",
+};
+
+const suites = [
+  {
+    params: {
+      sender: "0xE37129c296F3348ADDf6061E4dFB4e4f2385a86f",
+      x: -88,
+      y: 12,
+    },
+    block: 16473574,
+    expected: true,
+  },
+  {
+    params: {
+      sender: "0xa31f1f0d6bbd919bb3adab8da5835ed13e21f32a",
+      x: -17,
+      y: 37,
+    },
+    block: 16578039,
+    expected: true,
+  },
+  {
+    params: {
+      sender: "0xa31f1f0d6bbd919bb3adab8da5835ed13e21f32a",
+      x: -17,
+      y: 37,
+    },
+    block: 16578015,
+    expected: true,
+  },
+];
+
 async function main() {
-  const checkerAddress = ethers.Wallet.createRandom().address;
-  const checkerInterface = Checker__factory.createInterface();
+  for (let i = 0; i < suites.length; i++) {
+    try {
+      const { params, block, expected } = suites[i];
 
-  const res = await hre.network.provider.send("eth_call", [
-    {
-      to: checkerAddress,
-      data: checkerInterface.encodeFunctionData("checkLAND", [
-        "0xE37129c296F3348ADDf6061E4dFB4e4f2385a86f",
-        "0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d",
-        "0x959e104e1a4db6317fa58f8295f586e1a978c297",
-        -88,
-        12,
-      ]),
-    },
-    ethers.utils.hexStripZeros(ethers.utils.hexlify(16473574)),
-    {
-      [checkerAddress]: {
-        code: bytecode,
-      },
-    },
-  ]);
+      const hex = await hre.network.provider.send("eth_call", [
+        {
+          to: checkerAddress,
+          data: checkerInterface.encodeFunctionData("checkLAND", [
+            params.sender,
+            contracts.land,
+            contracts.estate,
+            params.x,
+            params.y,
+          ]),
+        },
+        ethers.utils.hexStripZeros(ethers.utils.hexlify(block)),
+        {
+          [checkerAddress]: {
+            code: bytecode,
+          },
+        },
+      ]);
 
-  console.log(checkerInterface.decodeFunctionResult("checkLAND", res));
+      const hasAccess = checkerInterface.decodeFunctionResult("checkLAND", hex)[0];
+
+      hasAccess === expected ? console.log("SUCCESS") : console.error("FAILURE");
+    } catch (e) {
+      console.error("FAILURE: ", (e as Error).message);
+    }
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere

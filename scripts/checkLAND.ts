efbcc1edd805b5bf3a0c4d508f50fe498084f107
Nando <24811313+fzavalia@users.noreply.github.com>
2023-02-12 18:45:23 -0300

    feat: Move tests to separate file

--- scripts/checkLAND.ts
+++ scripts/checkLAND.ts
@@ -1,6 +1,7 @@
 import hre, { ethers } from "hardhat";
 import { Checker__factory } from "../typechain-types";
 import { bytecode } from "./bytecode.json";
+import tests from "./checkLAND.tests.json";
 
 const checkerAddress = ethers.Wallet.createRandom().address;
 const checkerInterface = Checker__factory.createInterface();
@@ -10,40 +11,10 @@ const contracts = {
   estate: "0x959e104e1a4db6317fa58f8295f586e1a978c297",
 };
 
-const suites = [
-  {
-    params: {
-      sender: "0xE37129c296F3348ADDf6061E4dFB4e4f2385a86f",
-      x: -88,
-      y: 12,
-    },
-    block: 16473574,
-    expected: true,
-  },
-  {
-    params: {
-      sender: "0xa31f1f0d6bbd919bb3adab8da5835ed13e21f32a",
-      x: -17,
-      y: -37,
-    },
-    block: 16578039,
-    expected: true,
-  },
-  {
-    params: {
-      sender: "0xa31f1f0d6bbd919bb3adab8da5835ed13e21f32a",
-      x: -17,
-      y: -37,
-    },
-    block: 16578015,
-    expected: true,
-  },
-];
-
 async function main() {
-  for (let i = 0; i < suites.length; i++) {
+  for (let i = 0; i < tests.length; i++) {
     try {
-      const { params, block, expected } = suites[i];
+      const { params, block, expected } = tests[i];
 
       const hex = await hre.network.provider.send("eth_call", [
         {

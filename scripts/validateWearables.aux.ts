58fa2c95f0c1d1996d9b9004bce1fa7be3845d4d
Nando <24811313+fzavalia@users.noreply.github.com>
2023-02-15 14:37:16 -0300

    feat: Update aux file

--- scripts/validateWearables.aux.ts
+++ scripts/validateWearables.aux.ts
@@ -2,31 +2,34 @@ import { ethers } from "hardhat";
 import CollectionFactoryAbi from "../abis/CollectionFactory.json";
 import CommitteeAbi from "../abis/Committee.json";
 import CollectionAbi from "../abis/Collection.json";
+import tests from "./validateWearables.tests.json";
+
+const contracts = {
+  collectionFactoryV2: "0xb549b2442b2bd0a53795bc5cdcbfe0caf7aca9f8",
+  collectionFactoryV3: "0x3195e88ae10704b359764cb38e429d24f1c2f781",
+  oldCommittee: "0x71d9350ef44e1e451f00e447c0dff2d1fb75510a",
+  committee: "0xaeec95a8aa671a6d3fec56594827d7804964fa70",
+};
 
 async function main() {
-  const factoryV2Address = "0xb549b2442b2bd0a53795bc5cdcbfe0caf7aca9f8";
-  const factoryV3Address = "0x3195e88ae10704b359764cb38e429d24f1c2f781";
-  const collectionAddress = "0x336bd8da16807f8e542c83ab3789813c9a726ae6";
-  const oldCommitteeAddress = "0x71d9350ef44e1e451f00e447c0dff2d1fb75510a";
-  const committeeAddress = "0xaeec95a8aa671a6d3fec56594827d7804964fa70";
+  const test = tests[0];
 
-  const factoryV2 = new ethers.Contract(factoryV2Address, CollectionFactoryAbi, ethers.provider);
-  const factoryV3 = new ethers.Contract(factoryV3Address, CollectionFactoryAbi, ethers.provider);
-  const collection = new ethers.Contract(collectionAddress, CollectionAbi, ethers.provider);
-  const oldCommittee = new ethers.Contract(oldCommitteeAddress, CommitteeAbi, ethers.provider);
-  const committee = new ethers.Contract(committeeAddress, CommitteeAbi, ethers.provider);
+  const factoryV2 = new ethers.Contract(contracts.collectionFactoryV2, CollectionFactoryAbi, ethers.provider);
+  const factoryV3 = new ethers.Contract(contracts.collectionFactoryV3, CollectionFactoryAbi, ethers.provider);
+  const oldCommittee = new ethers.Contract(contracts.oldCommittee, CommitteeAbi, ethers.provider);
+  const committee = new ethers.Contract(contracts.committee, CommitteeAbi, ethers.provider);
+  const collection = new ethers.Contract(test.params.collection, CollectionAbi, ethers.provider);
 
-  const block = 39221633;
-  const options = { blockTag: block };
+  const options = { blockTag: test.block };
 
-  const sender = "0x5ce9fb617333b8c5a8f7787710f7c07002cb3516";
-  const itemId = "0";
-  const contentHash = "bafkreib45owymjtcipk5cthhjthg4mk4q5d4ecyv72i5rp352mvuneh2gm";
+  const sender = test.params.sender;
+  const itemId = test.params.itemId;
+  const contentHash = test.params.contentHash;
 
-  const isCollectionFromFactoryV2 = await factoryV2.isCollectionFromFactory(collectionAddress, options);
+  const isCollectionFromFactoryV2 = await factoryV2.isCollectionFromFactory(collection.address, options);
   console.log("isCollectionFromFactoryV2", isCollectionFromFactoryV2);
 
-  const isCollectionFromFactoryV3 = await factoryV3.isCollectionFromFactory(collectionAddress, options);
+  const isCollectionFromFactoryV3 = await factoryV3.isCollectionFromFactory(collection.address, options);
   console.log("isCollectionFromFactoryV3", isCollectionFromFactoryV3);
 
   const item = await collection.items(itemId, options);
@@ -59,7 +62,7 @@ async function main() {
   const isCollectionValid = !isApproved && isCompleted;
   console.log("isCollectionValid", isCollectionValid);
 
-  const addressHasAccess = creator === sender || globalManager || itemManager
+  const addressHasAccess = creator === sender || globalManager || itemManager;
   console.log("addressHasAccess", addressHasAccess);
 }
 

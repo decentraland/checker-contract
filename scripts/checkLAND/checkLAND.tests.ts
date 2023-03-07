96c58c7dbce7688ea0fd9b96f74fe1e380ae1b0e
Hugo Arregui <hugo@decentraland.org>
2023-03-07 11:56:41 -0300

    tests

--- scripts/checkLAND/checkLAND.tests.ts
+++ scripts/checkLAND/checkLAND.tests.ts
@@ -29,6 +29,33 @@ export function getTestsForNetwork(network: string) {
           block: 16578015,
           expected: true,
         },
+        {
+          params: {
+            sender: "0x717a0f32a82f715f3577856c86ada43fb600831a",
+            x: -67,
+            y: -111,
+          },
+          block: 16744822,
+          expected: true,
+        },
+        {
+          params: {
+            sender: "0x009249ad630734492f0081a09ed8c998c680ffe0",
+            x: 142,
+            y: -146,
+          },
+          block: 16750703,
+          expected: true,
+        },
+        {
+          params: {
+            sender: "0x40c6a0AbB1ED3caa6A56d5dE3410Af2e6088EA23",
+            x: 30,
+            y: 71,
+          },
+          block: 16501894,
+          expected: true,
+        },
       ];
     default:
       throw new Error(`Network ${network} not supported`);
